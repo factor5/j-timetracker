d3235cbe8c4965da559fd596a4ec1bc40483e6d6
svelikov <svelikov@ad4a7ab3-e939-bd0b-2b8d-c201ee5e7802>
2010-11-16 22:17:06 +0000

    add javadoc

--- trunk/TimeTracker/src/com/svelikov/timetracker/ui/ActionColumnRenderer.java
+++ trunk/TimeTracker/src/com/svelikov/timetracker/ui/ActionColumnRenderer.java
@@ -6,12 +6,19 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 
+/**
+ * Custom column renderer. Provides possibility for the JTable to render JPanel
+ * or JButon in its cells.
+ * 
+ * @author svelikov
+ * 
+ */
 public class ActionColumnRenderer extends JPanel implements TableCellRenderer {
 
 	@Override
-	public Component getTableCellRendererComponent(JTable table, Object value,
-			boolean isSelected, boolean hasFocus, int row, int column) {
-
+	public Component getTableCellRendererComponent(final JTable table,
+			final Object value, final boolean isSelected,
+			final boolean hasFocus, final int row, final int column) {
 		return (Component) value;
 	}
 
