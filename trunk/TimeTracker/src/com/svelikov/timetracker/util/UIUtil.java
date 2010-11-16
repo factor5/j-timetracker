c75d1aa486c1c70fa58a486eb0dfd5d73a1cef10
svelikov <svelikov@ad4a7ab3-e939-bd0b-2b8d-c201ee5e7802>
2010-11-16 22:29:17 +0000

    add javadoc

--- trunk/TimeTracker/src/com/svelikov/timetracker/util/UIUtil.java
+++ trunk/TimeTracker/src/com/svelikov/timetracker/util/UIUtil.java
@@ -6,6 +6,7 @@ package com.svelikov.timetracker.util;
 import java.awt.Container;
 import java.awt.GridLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -18,44 +19,66 @@ import com.svelikov.timetracker.action.TimeTrackerAction;
 import com.svelikov.timetracker.exception.UIInitializationException;
 
 /**
- * @author Factor5
+ * UI utility provider class.
+ * 
+ * @author svelikov
  */
 public class UIUtil {
 
+	/**
+	 * Private constructor.
+	 */
 	private UIUtil() {
 	}
 
-	public static void setColumnWidth(JTable table, final int columnNumber,
-			final int width) throws UIInitializationException {
-		TableColumnModel columnModel = table.getColumnModel();
+	/**
+	 * 
+	 * @param table
+	 * @param columnNumber
+	 * @param width
+	 * @throws UIInitializationException
+	 */
+	public static void setColumnWidth(final JTable table,
+			final int columnNumber, final int width)
+			throws UIInitializationException {
+		final TableColumnModel columnModel = table.getColumnModel();
 		if (columnNumber < 0 || columnNumber > columnModel.getColumnCount()) {
 			throw new UIInitializationException(
 					"Column number must be > 0 and < "
 							+ columnModel.getColumnCount());
 		}
-		TableColumn column = table.getColumnModel().getColumn(columnNumber);
+		final TableColumn column = table.getColumnModel().getColumn(
+				columnNumber);
 		column.setPreferredWidth(width);
 	}
 
+	/**
+	 * Creates a panel with action buttons to be placed inside every timer.
+	 * 
+	 * @return A JPanel with action buttons.
+	 */
 	public static JPanel getActionButtons() {
-		JPanel panel = new JPanel();
+		final JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(1, 3));
 
-		TimeTrackerAction actionListener = new TimeTrackerAction();
+		final TimeTrackerAction actionListener = new TimeTrackerAction();
 
-		JButton stopTimerActionButton = new JButton("stop");
+		final ImageIcon stopIcon = new ImageIcon("img/Aqua-Stop-icon.png");
+		final JButton stopTimerActionButton = new JButton(stopIcon);
 		stopTimerActionButton
 				.setActionCommand(ActionCommandConstants.STOP_TIMER);
 		panel.add(stopTimerActionButton);
 		stopTimerActionButton.addActionListener(actionListener);
 
-		JButton startTimerActionButton = new JButton("start");
+		final ImageIcon startIcon = new ImageIcon("img/Aqua-Play-icon.png");
+		final JButton startTimerActionButton = new JButton(startIcon);
 		startTimerActionButton
 				.setActionCommand(ActionCommandConstants.START_TIMER);
 		panel.add(startTimerActionButton);
 		startTimerActionButton.addActionListener(actionListener);
 
-		JButton removeTimerActionButton = new JButton("remove");
+		final ImageIcon deleteIcon = new ImageIcon("img/delete-icon.png");
+		final JButton removeTimerActionButton = new JButton(deleteIcon);
 		removeTimerActionButton
 				.setActionCommand(ActionCommandConstants.REMOVE_TIMER);
 		panel.add(removeTimerActionButton);
@@ -72,17 +95,17 @@ public class UIUtil {
 	 * @param msgType
 	 *            The type of the window to be shown.
 	 */
-	public static void setWarnings(Container parent, String warningMessage,
-			MessageType msgType) {
+	public static void setWarnings(final Container parent,
+			final String warningMessage, final MessageType msgType) {
 		switch (msgType) {
-			case WARNING:
-				JOptionPane.showMessageDialog(parent, warningMessage,
-						"Warning", JOptionPane.WARNING_MESSAGE);
-				break;
-			case INFORMATION:
-				JOptionPane.showMessageDialog(parent, warningMessage,
-						"Information", JOptionPane.INFORMATION_MESSAGE);
-				break;
+		case WARNING:
+			JOptionPane.showMessageDialog(parent, warningMessage, "Warning",
+					JOptionPane.WARNING_MESSAGE);
+			break;
+		case INFORMATION:
+			JOptionPane.showMessageDialog(parent, warningMessage,
+					"Information", JOptionPane.INFORMATION_MESSAGE);
+			break;
 		}
 	}
 
