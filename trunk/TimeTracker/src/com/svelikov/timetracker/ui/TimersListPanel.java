c8a8ec12e95778c9bb515ee484d9f519b9a217fa
svelikov <svelikov@ad4a7ab3-e939-bd0b-2b8d-c201ee5e7802>
2010-11-16 22:23:32 +0000

    add javadoc

--- trunk/TimeTracker/src/com/svelikov/timetracker/ui/TimersListPanel.java
+++ trunk/TimeTracker/src/com/svelikov/timetracker/ui/TimersListPanel.java
@@ -12,31 +12,44 @@ import com.svelikov.timetracker.exception.UIInitializationException;
 import com.svelikov.timetracker.util.UIUtil;
 
 /**
+ * A JTable wrapped in scroller pane.
+ * 
  * @author svelikov
  */
-public class TimersListPanel extends JPanel {
+public class TimersListPanel extends TimeTrackerExtendedPanel {
 
+	/**
+	 * This table.
+	 */
 	private JTable table;
 
-	public TimersListPanel(TimeTrackerTableModel tableModel) {
+	/**
+	 * Constructor that creates this component.
+	 * 
+	 * @param tableModel
+	 *            The table model to use for this table.
+	 */
+	public TimersListPanel(final TimeTrackerTableModel tableModel) {
 		table = new JTable(tableModel);
 		table.setPreferredScrollableViewportSize(new Dimension(575, 260));
 		table.setFillsViewportHeight(true);
 		table.setDefaultRenderer(JPanel.class, new ActionColumnRenderer());
 
 		try {
-			UIUtil.setColumnWidth(table, 0, 200);
+			UIUtil.setColumnWidth(table, 0, 350);
 			UIUtil.setColumnWidth(table, 1, 100);
-			UIUtil.setColumnWidth(table, 2, 200);
-		} catch (UIInitializationException e) {
-			e.printStackTrace();
+			UIUtil.setColumnWidth(table, 2, 50);
+		} catch (final UIInitializationException e) {
+			log.debug(e);
 		}
 
 		setLayout(new BorderLayout());
 		add(new JScrollPane(table));
-		Insets insets = getInsets();
-		Dimension size = getPreferredSize();
+		final Insets insets = getInsets();
+		final Dimension size = getPreferredSize();
 		setBounds(2 + insets.left, 35 + insets.top, size.width, size.height);
+
+		log.info("Created timers list table.");
 	}
 
 }
