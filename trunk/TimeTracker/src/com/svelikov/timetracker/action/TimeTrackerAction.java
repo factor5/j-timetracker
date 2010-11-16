3a9f4c7c8cbca63c725c6683add6ea127ad2d2e8
svelikov <svelikov@ad4a7ab3-e939-bd0b-2b8d-c201ee5e7802>
2010-11-16 17:20:52 +0000

    updated implementation

---
+++ trunk/TimeTracker/src/com/svelikov/timetracker/action/TimeTrackerAction.java
@@ -0,0 +1,27 @@
+package com.svelikov.timetracker.action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+
+import com.svelikov.timetracker.ActionCommandConstants;
+
+/**
+ * @author svelikov
+ */
+public class TimeTrackerAction extends WindowAdapter implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		if (ae.getActionCommand().equals(ActionCommandConstants.START_TIMER)) {
+			System.out.println("START");
+		} else if (ae.getActionCommand().equals(
+				ActionCommandConstants.STOP_TIMER)) {
+			System.out.println("STOP");
+		} else if (ae.getActionCommand().equals(
+				ActionCommandConstants.REMOVE_TIMER)) {
+			System.out.println("REMOVE");
+		}
+	}
+
+}
